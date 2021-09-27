1f0a0b60fcb358b04d45ec9c12c47ed5a30f573a
Aaron Russell Jr <adrusselljr@Aarons-MacBook-Pro.local>
2021-09-27 14:50:20 -0400

    added js

---
+++ main.js
@@ -0,0 +1,29 @@
+const prompt = require("prompt-sync")({ sigint: true })
+
+const random = Math.floor(Math.random() * 100)
+console.log(random)
+
+let guess = Number(prompt("I'm thinking of a number bewtween 1 and 100. Try to guess it! : "))
+let count = 0
+
+while(guess !== random) {
+
+    while(count !== 3) {
+
+        count++
+
+        if(guess > random) {
+            guess = Number(prompt("Sorry, too high! Guess Again! : "))
+        }
+        if(guess < random) {
+            guess = Number(prompt("Sorry, too low! Guess Again! : "))
+        }
+        if(count === 3) {
+            console.log(`Sorry i was thinking of ${random}`)
+            break
+        }
+
+    }
+
+}
+console.log("You guessed Right!")
